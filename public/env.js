88b6efe2ca5320cbcb6b34eaf519a27487d47d26
Cesar <cesar.kina@gmail.com>
2020-03-22 12:48:29 -0500

    Initial commit of components

---
+++ public/env.js
@@ -0,0 +1,8 @@
+// This is public! I couldn't hide this client id in the front end.. :(
+//    For now, I'll keep it here in a separate file, so that I can have
+//    one key for dev and another for prod
+const ENV = {
+    GITHUB: {
+        CLIENT_ID: '0f95382ee6992185ca65'
+    }
+};
