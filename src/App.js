ea3371c542c55a05e2d42562a5f4a48395928be2
Cesar <cesar.kina@gmail.com>
2020-03-24 19:19:24 -0500

    Transform React function-components to class-components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import Body from './Body';
 
-function App() {
-  return (
-    <div>
-      <Header />
-
-      <div className="mx-auto my-3 p-1" style={{maxWidth: '900px'}}>
-
-        <div id="login" className="blankslate">
-          <h3 className="mb-1">Welcome to gh-web-client</h3>
-          <p>You need to login with your Github account to see your content.</p>
-          <a href="/#" id="loginBtn" className="btn btn-primary my-3">Login</a>
-        </div>
-
-        <div id="main">
-          <div className="pagehead">
-            <h1>Pull Requests</h1>
-          </div>
-
-          <div className="Box Box--condensed">
-            <div className="Box-header">
-              <h3 className="Box-title">
-                PRs
-              </h3>
-            </div>
-            <ul id="pr-list">
-            </ul>
-          </div>
-        </div>
-        
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Body />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Body.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Body extends React.Component {
+    render() {
+        return (
+            <div className="mx-auto my-3 p-1" style={{maxWidth: '900px'}}>
+                <div id="login" className="blankslate">
+                    <h3 className="mb-1">Welcome to gh-web-client</h3>
+                    <p>You need to login with your Github account to see your content.</p>
+                    <a href="/#" id="loginBtn" className="btn btn-primary my-3">Login</a>
+                </div>
+        
+                <div id="main">
+                    <div className="pagehead">
+                    <h1>Pull Requests</h1>
+                    </div>
+        
+                    <div className="Box Box--condensed">
+                    <div className="Box-header">
+                        <h3 className="Box-title">
+                        PRs
+                        </h3>
+                    </div>
+                    <ul id="pr-list">
+                    </ul>
+                    </div>
+                </div>
+            </div>
+          );
+    }
+}
+
+export default Body;
