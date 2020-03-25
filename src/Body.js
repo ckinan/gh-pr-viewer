4236611972072fe929bd1c7c2097105f2a78e180
Cesar <cesar.kina@gmail.com>
2020-03-25 18:14:49 -0500

    Minor changes on some titles, descriptions, texts

--- src/Body.js
+++ src/Body.js
@@ -5,7 +5,7 @@ class Body extends React.Component {
         return (
             <div className="mx-auto my-3 p-1" style={{maxWidth: '900px'}}>
                 <div id="login" className="blankslate">
-                    <h3 className="mb-1">Welcome to gh-web-client</h3>
+                    <h3 className="mb-1">Welcome to Github Web Client</h3>
                     <p>You need to login with your Github account to see your content.</p>
                     <a href="/#" id="loginBtn" className="btn btn-primary my-3">Login</a>
                 </div>
