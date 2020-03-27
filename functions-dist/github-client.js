9923db8fc1e32e449fc044a3b5687d1dc7991692
Cesar <cesar.kina@gmail.com>
2020-03-26 19:41:45 -0500

    WIP: Read and write cookies

--- functions-dist/github-client.js
+++ functions-dist/github-client.js
@@ -2,13 +2,29 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const cookie = require('cookie');
+
+const IS_PRODUCTION = process.env.NETLIFY_DEV === 'false';
+
 exports.handler = async function(event) {
   const code = event.queryStringParameters.code;
   const params = new URLSearchParams();
   params.append('client_secret', GITHUB_CLIENT_SECRET);
   params.append('client_id', GITHUB_CLIENT_ID);
   params.append('code', code);
+
+  const cookies = event.headers.cookie && cookie.parse(event.headers.cookie)
+  console.log(cookies.accessTokenGH);
+  console.log(process.env);
+
+  const thirtyDays = 30 * 24 * 60 * 60;
+  const myCookie = cookie.serialize('my_cookie', 'lolHi', {
+    secure: IS_PRODUCTION,
+    httpOnly: true,
+    sameSite: true,
+    maxAge: thirtyDays,
+  })
 
   try {
     const response = await fetch(GITHUB_ACCESS_TOKEN_URL, {
@@ -20,7 +36,10 @@ exports.handler = async function(event) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data })
+      body: JSON.stringify({ msg: data }),
+      'headers': {
+        'Set-Cookie': myCookie
+      }
     };
   } catch (err) {
     console.log(err); // output to netlify function log
