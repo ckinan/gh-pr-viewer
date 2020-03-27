47cdbc8b24402076946922d57cb258cb146d5c71
Cesar <cesar.kina@gmail.com>
2020-03-27 18:12:35 -0500

    Extract auth cookies logic to a module

---
+++ functions-dist/commons/auth.js
@@ -0,0 +1,20 @@
+const cookie = require('cookie');
+
+const IS_PRODUCTION = process.env.NETLIFY_DEV === 'false';
+
+exports.create = (token) => {
+  const thirtyDays = 30 * 24 * 60 * 60;
+  const newCookie = cookie.serialize('ghAccessToken', token, {
+    secure: IS_PRODUCTION,
+    httpOnly: true,
+    sameSite: true,
+    maxAge: thirtyDays,
+  });
+  return newCookie;
+}
+
+exports.check = (event) => {
+  const cookies = event.headers.cookie && cookie.parse(event.headers.cookie);
+  console.log(cookies.ghAccessToken);
+  return !!cookies.ghAccessToken;
+}
--- functions-dist/github-client.js
+++ functions-dist/github-client.js
@@ -3,9 +3,7 @@ const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 
 const fetch = require('node-fetch');
-const cookie = require('cookie');
-
-const IS_PRODUCTION = process.env.NETLIFY_DEV === 'false';
+const auth = require('./commons/auth');
 
 exports.handler = async function(event) {
   const code = event.queryStringParameters.code;
@@ -14,17 +12,7 @@ exports.handler = async function(event) {
   params.append('client_id', GITHUB_CLIENT_ID);
   params.append('code', code);
 
-  const cookies = event.headers.cookie && cookie.parse(event.headers.cookie)
-  console.log(cookies.accessTokenGH);
-  console.log(process.env);
-
-  const thirtyDays = 30 * 24 * 60 * 60;
-  const myCookie = cookie.serialize('my_cookie', 'lolHi', {
-    secure: IS_PRODUCTION,
-    httpOnly: true,
-    sameSite: true,
-    maxAge: thirtyDays,
-  })
+  console.log(`Is token valid? ${auth.check(event)}`);
 
   try {
     const response = await fetch(GITHUB_ACCESS_TOKEN_URL, {
@@ -38,7 +26,7 @@ exports.handler = async function(event) {
       statusCode: 200,
       body: JSON.stringify({ msg: data }),
       'headers': {
-        'Set-Cookie': myCookie
+        'Set-Cookie': auth.create(data.access_token)
       }
     };
   } catch (err) {
