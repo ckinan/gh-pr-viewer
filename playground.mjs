9487b0f50a1aa1c8405d368dcfaae08c0ca43266
Cesar <cesar.kina@gmail.com>
2020-04-02 18:38:33 -0500

    Dates fromNow()

---
+++ playground.mjs
@@ -0,0 +1,3 @@
+import { moment } from 'moment';
+
+console.log(moment('1995-12-25'));
--- src/client/PullRequestBoxRow.js
+++ src/client/PullRequestBoxRow.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Octicon, { GitMerge, GitPullRequest } from '@primer/octicons-react';
+import moment from 'moment';
 
 const PullRequestBoxRow = props => {
   return (
     <li className="Box-row">
       <div>
-        <div class="d-table-cell">
-          {props.pr.state == 'MERGED' ? (
+        <div className="d-table-cell">
+          {props.pr.state === 'MERGED' ? (
             <Octicon icon={GitMerge} className="text-purple" />
-          ) : props.pr.state == 'OPEN' ? (
+          ) : props.pr.state === 'OPEN' ? (
             <Octicon icon={GitPullRequest} className="text-green" />
           ) : (
             <Octicon icon={GitPullRequest} className="text-red" />
           )}
         </div>
-        <div class="d-table-cell pl-2">
-          <div className="mb-2">
+        <div className="d-table-cell pl-2">
+          <div className="mb-1">
             <strong>
               <a
                 href={props.pr.repository.url}
@@ -31,27 +32,59 @@ const PullRequestBoxRow = props => {
                 rel="noopener noreferrer"
                 className="ml-2 text-gray-dark"
               >
-                #{props.pr.number} {props.pr.title} ({props.pr.state})
+                #{props.pr.number} {props.pr.title}
               </a>
             </strong>
           </div>
 
-          <div className="text-small text-gray-light">
-            <div>
+          <div className="d-table text-small text-gray-light">
+            <div className="float-md-left pr-3">
               <strong>Created at</strong>:{' '}
-              {new Date(props.pr.createdAt).toString()}
+              <span
+                className="tooltipped tooltipped-s"
+                aria-label={new Date(props.pr.createdAt).toString()}
+              >
+                {moment.utc(props.pr.createdAt).fromNow()}
+              </span>
             </div>
-            <div>
+            <div className="float-md-left pr-3">
               <strong>Updated at</strong>:{' '}
-              {new Date(props.pr.updatedAt).toString()}
+              <span
+                className="tooltipped tooltipped-s"
+                aria-label={new Date(props.pr.updatedAt).toString()}
+              >
+                {moment.utc(props.pr.updatedAt).fromNow()}
+              </span>
             </div>
-            <div>
+            <div className="float-md-left pr-3">
               <strong>Closed at</strong>:{' '}
-              {props.pr.closedAt ? new Date(props.pr.closedAt).toString() : '-'}
+              <span
+                className="tooltipped tooltipped-s"
+                aria-label={
+                  props.pr.closedAt
+                    ? new Date(props.pr.closedAt).toString()
+                    : '-'
+                }
+              >
+                {props.pr.closedAt
+                  ? moment.utc(props.pr.closedAt).fromNow()
+                  : '-'}
+              </span>
             </div>
-            <div>
+            <div className="float-md-left">
               <strong>Merged at</strong>:{' '}
-              {props.pr.mergedAt ? new Date(props.pr.mergedAt).toString() : '-'}
+              <span
+                className="tooltipped tooltipped-s"
+                aria-label={
+                  props.pr.mergedAt
+                    ? new Date(props.pr.mergedAt).toString()
+                    : '-'
+                }
+              >
+                {props.pr.mergedAt
+                  ? moment.utc(props.pr.mergedAt).fromNow()
+                  : '-'}
+              </span>
             </div>
           </div>
         </div>
