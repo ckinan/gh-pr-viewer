2b74406e2fbc3fe4164f378684d1e300da545408
Cesar <cesar.kina@gmail.com>
2020-04-08 18:28:02 -0500

    Convert ProtectedView.js from class to functional component

--- src/client/ProtectedView.js
+++ src/client/ProtectedView.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PullRequestBox from './PullRequestBox';
 
-class ProtectedView extends React.Component {
-  render() {
-    return (
-      <div className="mx-auto my-3 p-1" style={{ maxWidth: '900px' }}>
-        <PullRequestBox />
-      </div>
-    );
-  }
+const ProtectedView = () => {
+  return (
+    <div className="mx-auto my-3 p-1" style={{ maxWidth: '900px' }}>
+      <PullRequestBox />
+    </div>
+  );
 }
 
 export default ProtectedView;
