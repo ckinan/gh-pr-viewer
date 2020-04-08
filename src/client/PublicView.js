208b89260f5c1e076ee74ec842e7d23d86c8f533
Cesar <cesar.kina@gmail.com>
2020-04-08 18:29:00 -0500

    Convert PublicView.js from class to functional component

--- src/client/PublicView.js
+++ src/client/PublicView.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-class LoginPage extends React.Component {
-  render() {
-    return (
-      <div className="mx-auto my-3 p-1" style={{ maxWidth: '900px' }}>
-        <div className="blankslate">
-          <h3 className="mb-1">Welcome to Github Web Client</h3>
-          <p>You need to login with your Github account to see your content.</p>
-          <a
-            href="https://github.com/login/oauth/authorize?scope=user:email,read:org&client_id=0f95382ee6992185ca65"
-            className="btn btn-primary my-3"
-          >
-            Login
-          </a>
-        </div>
+const LoginPage = () => {
+  return (
+    <div className="mx-auto my-3 p-1" style={{ maxWidth: '900px' }}>
+      <div className="blankslate">
+        <h3 className="mb-1">Welcome to Github Web Client</h3>
+        <p>You need to login with your Github account to see your content.</p>
+        <a
+          href="https://github.com/login/oauth/authorize?scope=user:email,read:org&client_id=0f95382ee6992185ca65"
+          className="btn btn-primary my-3"
+        >
+          Login
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default LoginPage;
