cb08d790de6b58b5aa1a322ff3057fd7ee35a36f
Cesar <cesar.kina@gmail.com>
2020-04-08 18:42:50 -0500

    Hooks in PullRequestBoxHeader.js

--- src/client/PullRequestBoxHeader.js
+++ src/client/PullRequestBoxHeader.js
@@ -1,46 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Octicon, { GitPullRequest } from '@primer/octicons-react';
 
-class PullRequestBoxHeader extends React.Component {
-  state = {
-    user: 'ckinan',
+const PullRequestBoxHeader = ({fetchPullRequests, prs}) => {
+
+  const [user, setUser] = useState('ckinan');
+
+  const handleChange = (e) => {
+    setUser(e.target.value);
   };
 
-  handleChange = (e) => {
-    this.setState({ user: e.target.value });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.fetchPullRequests(this.state.user);
+    fetchPullRequests(user);
   };
 
-  componentDidMount() {
-    this.props.fetchPullRequests(this.state.user);
-  }
+  useEffect(() => {
+    fetchPullRequests(user);
+  }, []);
 
-  render() {
-    return (
-      <div className="Box-header">
-        <h3 className="Box-title">
-          <div className="mr-2">
-            <form onSubmit={(e) => this.handleSubmit(e)}>
-              <Octicon icon={GitPullRequest} className="text-green mr-1" />
-              Open
-              <span className="Counter ml-1">{this.props.prs.length}</span>
-              <input
-                className="form-control ml-2"
-                type="text"
-                placeholder="User"
-                value={this.state.user}
-                onChange={(e) => this.handleChange(e)}
-              />
-            </form>
-          </div>
-        </h3>
-      </div>
-    );
-  }
+  return (
+    <div className="Box-header">
+      <h3 className="Box-title">
+        <div className="mr-2">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <Octicon icon={GitPullRequest} className="text-green mr-1" />
+            Open
+            <span className="Counter ml-1">{prs.length}</span>
+            <input
+              className="form-control ml-2"
+              type="text"
+              placeholder="User"
+              value={user}
+              onChange={(e) => handleChange(e)}
+            />
+          </form>
+        </div>
+      </h3>
+    </div>
+  );
 }
 
 export default PullRequestBoxHeader;
