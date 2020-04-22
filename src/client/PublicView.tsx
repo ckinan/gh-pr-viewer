d530e3c7b96eb8864a6aeb86d6d99206a55944c4
Cesar <cesar.kina@gmail.com>
2020-04-21 20:17:28 -0500

    Update client id

--- src/client/PublicView.tsx
+++ src/client/PublicView.tsx
@@ -8,7 +8,7 @@ const LoginPage: React.FC = () => {
         <h3 className="mb-1">Welcome to Github Pull Request Viewer</h3>
         <p>You need to login with your Github account to see your content.</p>
         <a
-          href="https://github.com/login/oauth/authorize?scope=user:email,read:org&client_id=0f95382ee6992185ca65"
+          href="https://github.com/login/oauth/authorize?scope=user:email,read:org&client_id=9ef2db5a41ff672e12c1"
           className="btn btn-primary my-3"
         >
           Login
